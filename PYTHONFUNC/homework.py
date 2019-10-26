ebb07deae0438045ba35c70ac7658f4317f78dfc
annayakusenko <anna.yakusenko@ukr.net>
2019-10-27 00:18:31 +0300

    test

---
+++ PYTHONFUNC/homework.py
@@ -0,0 +1,133 @@
+from typing import List, Dict, Union, Generator
+
+# We will work with such dicts
+ST = Dict[str, Union[str, int]]
+# And we will put this dicts in list
+DT = List[ST]
+
+
+def task_1_fix_names_start_letter(data: DT) -> DT:
+    """
+    Make all `names` field in list of students to start from upper letter
+
+    Examples:
+        fix_names_start_letters([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}])
+        >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
+    """
+    for i in data:
+        if 'name' in i:
+            i['name'] = i['name'].capitalize()
+    return data
+
+
+def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
+    """given_data
+    Remove from dictionaries given key value
+
+    Examples:
+       remove_dict_field([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
+        >>> [{'name': 'Alex'}, {'name': 'denys'}]
+    """
+    return[{key: value for key, value in i.items() if key not in ['sex', 'name']}for i in data]
+
+
+def task_3_find_item_via_value(data: DT, value) -> DT:
+    """
+    Find and return all items that has @searching value in any key
+    Examples:
+        find_item_via_value([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
+        >>> [{'name': 'Alex', 'age': 26}]
+    """
+    return [i for i in data if value in i.values()]
+
+
+def task_4_min_value_integers(data: List[int]) -> int:
+    """
+    Find and return minimum value from list
+    """
+    return min(data) if len(data) >= 1 else None
+
+
+def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
+    """
+    Find the longest string
+    """
+    if len(data) > 0:
+        if isinstance(data, str):
+            data = data.split()
+        data = map(str, data)
+        data = sorted(data, key=len)
+        return data[0]
+
+
+def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
+    """
+    Find minimum value by given key
+    Returns:
+
+    """
+    min_value = data[0]
+    for i in data:
+        if 'age' in i.keys():
+            if i['age'] < min_value['age']:
+                min_value = i
+    return min_value
+
+
+def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
+    """
+    Find max value from list of lists
+    """
+    new_list = []
+    for i in data:
+        for j in i:
+            new_list.append(j)
+    return sorted(new_list)[-1]
+
+def task_8_sum_of_ints(data: List[int]) -> int:
+    """
+    Find sum of all items in given list
+    """
+    return sum(data)
+
+
+def task_9_sum_characters_positions(text: str) -> int:
+    """
+    Please read first about ascii table.
+    https://python-reference.readthedocs.io/en/latest/docs/str/ASCII.html
+    You need to calculate sum of decimal value of each symbol in text
+
+    Examples:
+        task_9_sum_characters_positions("A")
+        >>> 65
+        task_9_sum_characters_positions("hello")
+        >>> 532
+
+    """
+    int_list = []
+    for i in text:
+        i = ord(i)
+        int_list.append(i)
+    return sum(int_list)
+
+
+def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
+    """
+    Return generator of simple numbers
+    Stop then iteration if returned value is more than 200
+    Examples:
+        a = task_10_generator_of_simple_numbers()
+        next(a)
+        >>> 2
+        next(a)
+        >>> 3
+    """
+    number = 2
+    while number <= 200:
+        for i in range(2, number):
+            if number % i == 0:
+                number += 1
+                break
+        else:
+            yield number
+            number += 1
